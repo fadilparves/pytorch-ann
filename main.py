0ad26c54d67da37a74d12f72976c485c8c8b310f
fadilparves <fadil.parves@gmail.com>
2020-10-13 20:59:12 +0800

    init model file with imports

---
+++ main.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+import os
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+sns.set(style='whitegrid', palette='muted', font_scale=1.2)
+
+HAPPY_COLORS_PALETTE = ["#01BEFE", "#FFDD00", "#FF7D00", "#FF006D", "#93D30C", "#8F0\
+0FF"]
+
+sns.set_palette(sns.color_palette(HAPPY_COLORS_PALETTE))
+
+rcParams['figure.figsize'] = 12, 8
+
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+torch.manual_seed(RANDOM_SEED)
